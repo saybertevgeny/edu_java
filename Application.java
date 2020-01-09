03a1c318aba632c36541ddfa99400b82a36e4624
Evgeny Saybert <saybert@lanit.ru>
2020-01-09 14:24:03 +0300

    Переменное количество аргументов

--- Application.java
+++ Application.java
@@ -1,4 +1,9 @@
 public class Application{
     public static void main(String[] args) {
+        VarArgs varArgs = new VarArgs();
+        varArgs.test();
+        varArgs.test("Первый аргумент");
+        varArgs.test("Первый аргумент","Второй аргумент");
+        varArgs.test("Первый аргумент","Второй аргумент","Третий аргумент");
     }
 }
---
+++ src/VarArgs.java
@@ -0,0 +1,18 @@
+public class VarArgs {
+
+    public void test(){
+        System.out.println("Аргументы не переданы");
+    }
+
+    public void test(String arg){
+        System.out.println("Передан один аргумент: " + arg);
+    }
+
+    public void test(String arg1,String arg2){
+        System.out.println("Передано два аргумента: " + arg1 + ", " + arg2);
+    }
+
+    public void test(String ...args){
+        System.out.println("Переданно разное количество аргументов:" + args.length);
+    }
+}
