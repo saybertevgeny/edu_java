5790f5cb0f80d18c49e3268564fef180ef6ed142
Evgeny Saybert <saybert@lanit.ru>
2020-01-09 13:13:53 +0300

    Инициализация статических переменных. Эдакий статический конструктор. Статический конструктор вызовется один раз при обращении к статическому полю или методу

--- Application.java
+++ Application.java
@@ -1,5 +1,7 @@
 public class Application{
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println(UseStatic.a);
         UseStatic.meth(25);
         UseStatic.meth(26);
 
