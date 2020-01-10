00f24c16a9b323bae112cdf228e72d827551c254
Evgeny Saybert <saybert@lanit.ru>
2020-01-10 17:15:06 +0300

    Повтораяющаяся анотация

--- src/Meta.java
+++ src/Meta.java
@@ -1,16 +1,18 @@
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 public class Meta {
 
     @MyAnno(name = "test")
+    @MyAnno(name = "test1")
     public static void meta(String str,int i){
 
         Meta ob = new Meta();
         try {
             Class<?> c = ob.getClass();
             Method method = c.getMethod("meta", String.class, int.class);
-            MyAnno my = method.getAnnotation(MyAnno.class);
-            System.out.println("Значение из анотации: " + my.name());
+            MyAnno[] my = method.getAnnotationsByType(MyAnno.class);
+            System.out.println("Значение из анотации: " + my);
         }
         catch (NoSuchMethodException e){
             System.out.println("Метод не найден");
